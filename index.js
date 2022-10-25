b886da962fd3ef94f21afc12db7dab688b753c16
MD Pahlovi <mdpahlovi07@gmail.com>
2022-10-25 11:20:24 +0600

    Courses data added

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require("./Data/courses.json");
+
 app.get("/", (req, res) => {
     res.send("Madroos Server Running");
+});
+
+app.get("/courses", (req, res) => {
+    res.send(courses);
 });
 
 app.listen(port, () => {
