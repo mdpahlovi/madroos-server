a13776b608cf3e6ca4e743243955325c81fde2c2
MD Pahlovi <mdpahlovi07@gmail.com>
2022-10-25 12:10:02 +0600

    Cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 const courses = require("./Data/courses.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Madroos Server Running");
